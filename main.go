bcdaacd75ab871d030b1410f470b69005a8b0851
DESKTOP-8D2LKCJ\Anton <zagco@ya.ru>
2019-02-19 22:40:50 +1000

    init

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/AntonVTR/probation_test/grapthql_fasthttp/testutil"
+
+	handler "github.com/lab259/graphql-fasthttp-handler"
+	"github.com/valyala/fasthttp"
+)
+
+func main() {
+	h := handler.New(&handler.Config{
+		Schema:   &testutil.StarWarsSchema,
+		Pretty:   true,
+		GraphiQL: true,
+	})
+
+	fasthttp.ListenAndServe(":8080", func(ctx *fasthttp.RequestCtx) {
+		h.ServeHTTP(ctx)
+	})
+}
